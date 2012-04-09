92cbe942907b0a22f6f972c116811de8fe826060
Phil Bird <Phil.bird@gmail.com>
2012-04-09 19:00:10 +0100

    Very basic working version, but with hard coded links. Should be ready for an alpha release in 3 or 4 days time.

--- TrophyUpdater/TrophyUpdater/Form1.cs
+++ TrophyUpdater/TrophyUpdater/Form1.cs
@@ -21,7 +21,9 @@ namespace TrophyUpdater
             startstop.AccessPoint oAccessPoint = new startstop.AccessPoint();
 
             psnapi.psn oPSN = new psnapi.psn();
-            psnapi.Game[] oGames = oPSN.getGames(textBox1.Text); 
+            psnapi.Game[] oGames = oPSN.getGames(textBox1.Text);
+            psnapi.psn oPSN2 = new psnapi.psn();
+            psnapi.psn oPSN3 = new psnapi.psn(); 
 
             foreach (psnapi.Game oGame in oGames)
             {
@@ -34,7 +36,42 @@ namespace TrophyUpdater
                 oResponse =  oAccessPoint.AddStatOverview("", oOverview);
                 if (oResponse.Success)
                 {
-                    // It has been added or updated. 
+                    try
+                    {
+                        // It has been added or updated. 
+                        // Now get a list of trophies
+                        psnapi.Trophy[] oTrophies = oPSN2.getListTrophies( oGame.Id);
+                        foreach (psnapi.Trophy oTrophy in oTrophies)
+                        {
+                            // Go through each one. 
+                            // Add it to the list
+                            startstop.Trophy ssTrophy = new startstop.Trophy();
+                            ssTrophy.Approved = true;
+                            ssTrophy.Name = oTrophy.Title;
+                            ssTrophy.Description = oTrophy.Description;
+                            startstop.MessageResponse _addTrophyResponse = oAccessPoint.UpdateTrophy("", ssTrophy);
+
+
+                            
+
+
+                        }
+                        
+                        // Now award trophies. 
+                        psnapi.Trophy[] oTrophiesAwarded = oPSN3.getTrophies(textBox1.Text, oGame.Id);
+                        foreach (psnapi.Trophy oTrophy in oTrophiesAwarded)
+                        {
+                            if (oTrophy.DateEarned.HasValue)
+                            {
+                                // We need to lookup the startstop user. 
+                                startstop.MessageResponse _awardtrophy = oAccessPoint.AwardTrophyByName("", 1, oTrophy.Title, oTrophy.DateEarned.GetValueOrDefault()); 
+                            }
+                        }
+
+                    }
+                    catch (Exception eX)
+                    {
+                    }
 
                 }
             }
